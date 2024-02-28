e04e8183582ca29a2c09fd7cc059686735d73836
TlonUqbar <125710315+TlonUqbar@users.noreply.github.com>
2024-02-27 21:32:12 -0800

    feat: Initial Commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './style.css'
+import _ from 'lodash'
+
+console.log(_.toUpper("hello world!"));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [ 
+    new HtmlWebpackPlugin({
+      template:  './src/index.html'
+    }), 
+    new BundleAnalyzerPlugin()],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    compress: true,
+    port: 3025
+  }
+}
